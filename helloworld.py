83dcef0869a8d2d0acc7dcaf237b2537ecb011bc
Palak Goyal <palak29062002@gmail.com>
2025-07-30 15:35:33 +0530

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,80 @@
+print("hello world")
+print('''hello
+      world''')
+#operators-operators are specail symbol which are used to perform operation or an operands
+#-------------------------------------------------------------------------------------------------------
+#Arithmetic operator{Addtion,subtraction,multipliation,Division,Modules/Remainder,Integer(floor) Division(quotient),Exponent/power}
+print(6+2) #output 8
+print(15/2) #output 7.5
+print(15%2) #ouput 1
+#Use operator precedence
+print(12+(3**4-6)/2) #output 49.5
+#------------------------------------------------------------------------------------------------------------
+#Relational operator(==,>,<,>=,<=,!=)
+a=3
+b=3
+if a == b:
+  print("equal")
+else:
+  print("Not Equal") #output Not Equal
+
+print(5>=3) #Output True
+print(5<=3)  #Output False
+#------------------------------------------------------------------------------------------------------------------------------
+#Logical operator(AND,OR,NOT)
+a=4
+b=5
+c=5
+print(a<b and b<c) #output False
+print(a<b or b<c) #output True
+print(not b<c) #output true
+#------------------------------------------------------------------------------------------------------------------------------
+#Assignment Operator(+= ,-+ ,*= ,/= ,//= ,%= ,**=)
+x=4
+x +=3
+print (x) #output 7
+x -+ 3
+print (x) #output 7
+x /= 3 
+print (x) #output 2.3333333333333335
+#------------------------------------------------------------------------------------------------------------------------------------
+#Identify operator(is, is not)
+a=3
+b=4
+print(a is b) #output True
+print(a is not b) #output False
+#--------------------------------------------------------------------------------------------------------------------------------
+#Membership opertor(in,not in)
+a=3
+b=[1,2,3,4,5]
+print(a in b) #output True
+print(a not in b) #output False
+#-------------------------------------------------------------------------------------------------------------------
+
+#Bitwise operator(&,^,|,~)
+c=4
+d=3
+print(c&d) #output 0
+print(c^d) #output 7
+print(c|d) #output 7
+print(~d) #ouput -4
+
+#-------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
